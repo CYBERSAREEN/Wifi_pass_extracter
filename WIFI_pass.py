a4733ad0c856ad179e1e5ca909ffda986a1d2954
VEDANT SAREEN <131311202+CYBERSAREEN@users.noreply.github.com>
2024-01-04 09:46:43 +0530

    Update WIFI_pass.py

--- WIFI_pass.py
+++ WIFI_pass.py
@@ -15,6 +15,8 @@ parser = opt.OptionParser() # the class is bieng called
 parser.add_option("-s","--show",action = 'store_true' ,dest = "show",help=" Will show all the saved network interfaces")
 parser.add_option("-n","--name",dest = "name",help=" Will allocate the 'NAME OF ACCESS POINT'")
 parser.add_option("-c","--current",action = 'store_true',dest = "current",help=" Will show the password of the current connected network")
+parser.add_option("-e","--export",action = 'store_true',dest = "export",help=" Will export all the password creating a pass folder")
+
 
 # We need to make the parser understand that how the code will work and where to innitialise the value/string added to it 
 (options,arguments) = parser.parse_args() #it will act as function 
@@ -44,6 +46,11 @@ if options.show:
 elif options.name:
     result_concluder(options.name)
 
+# Export all profiles creating a file pass on the directory
+elif options.export:
+    sb.call(['mkdir','pass'])
+    sb.call(['netsh', 'wlan', 'export', 'profile','folder=pass','key=clear'])
+
 # Display the password for the current connected ACCESS POINT
 elif options.current:
     current_profile = sb.check_output(['netsh', 'wlan', 'show', 'interfaces'], text=True)
@@ -54,8 +61,12 @@ elif options.current:
             profile_name = line.split(':')[1].strip()
             break
 
+
 # Basic python string based program that will extract the name of profile and then calls out the function result_concluder replacing the x value with prifile name. 
     if profile_name:
         result_concluder(profile_name)
     else:
         print("Not currently connected to a Wi-Fi network.")
+
+        
+        
